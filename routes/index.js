4ebf1c7e09392e5ad7d7aad6ac94ef51239546f8
Ed Botzum <contact@edbotz.us>
2014-09-11 20:50:51 -0400

    Initial Build of Camera Man for RasbPi

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
+
+/* Take picture and load home page */
+router.get('/', function(req, res) {
+  exec('ping -c4 google.com', function(error, stdout, stderr) {
+  //exec('raspistill -o public/images/fuel.jpg', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+    res.render('index', { title: 'Camera Man' })
+  });
+});
+
+module.exports = router;
